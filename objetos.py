517e850a41c9319e2f22df54d020c6ee8fe04140
Marcos Gutiérrez <marcos.gutierrez.hernandez@gmail.com>
2019-07-29 17:54:42 +0200

    objetos, turtle

---
+++ objetos.py
@@ -0,0 +1,11 @@
+import turtle
+
+tortuguita = turtle.Turtle()
+otraTortuguita = turtle.Turtle()
+
+tortuguita.color('green')
+tortuguita.shape('turtle')
+tortuguita.fd(50)
+
+otraTortuguita.left(90)
+otraTortuguita.fd(50)
