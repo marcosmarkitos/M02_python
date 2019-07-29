9ba72cebdb6af5ea7a20bcfcfcea6b90c6d33db4
Marcos Gutiérrez <marcos.gutierrez.hernandez@gmail.com>
2019-07-29 16:50:19 +0200

    funciones operador map filter reduce

---
+++ mapFilterReduce.py
@@ -0,0 +1,16 @@
+from functools import reduce
+
+def doble(x):
+    return (x*2)
+
+
+lista = [1, 3, -1, 15, 9]
+
+listaDobles = map (lambda x: x*2, lista)
+listaDobles1 = map (doble, lista)
+
+listaPares = filter (lambda x: x % 2 == 0, lista)
+listaMultiplosDeTres = filter (lambda x: x % 3 == 0, lista)
+
+sumatorio = reduce (lambda x, y: x+y, lista)
+suma100 = reduce (lambda x, y: x+y, range(101))
